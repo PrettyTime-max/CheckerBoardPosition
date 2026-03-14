0ba9a4ffa3c8868532ad6915d56f4bad1e903f2d
PrettyTime-max <89823352057@mail.ru>
2026-03-14 09:45:49 +0500

    Add tests for public byte X public byte Y constructor

--- CheckerBoardPositionTest/UnitTest1.cs
+++ CheckerBoardPositionTest/UnitTest1.cs
@@ -25,4 +25,64 @@ public class MovementColumn
         Assert.Equal(post1[0] - post2[0], deltaX);
         Assert.Equal(post1[1] - post2[1], deltaY);
     }
+    [Theory]
+    [InlineData(1,2)]
+    [InlineData(5,3)]
+    [InlineData(6,2)]
+    [InlineData(8,4)]
+    [InlineData(6,8)]
+    public void Constructor_ValidPosition_ReturnCorrectPositionOnX(byte x, byte y)
+    {
+        var position = new CheckerBoardPosition(x, y);
+        Assert.Equal(x, position.X);
+    }
+    [Theory]
+    [InlineData(9, 2)]
+    [InlineData(0, 3)]
+    [InlineData(10, 2)]
+    [InlineData(11, 4)]
+    public void Constructur_NotValidPosition_ReturnArgumentOutOfRangeExceptionOnX(byte x, byte y)
+    {
+        Assert.Throws<ArgumentOutOfRangeException>(() => new CheckerBoardPosition(x,y));
+    }
+    [Theory]
+    [InlineData(1, 11)]
+    [InlineData(4, 10)]
+    [InlineData(6, 9)]
+    [InlineData(8, 0)]
+    public void Constructur_NotValidPosition_ReturnArgumentOutOfRangeExceptionOnY(byte x, byte y)
+    {
+        Assert.Throws<ArgumentOutOfRangeException>(() => new CheckerBoardPosition(x, y));
+    }
+    [Theory]
+    [InlineData(1, 2)]
+    [InlineData(5, 3)]
+    [InlineData(6, 2)]
+    [InlineData(8, 4)]
+    [InlineData(6, 8)]
+    public void Constructor_ValidPosition_ReturnCorrectPositionOnY(byte x, byte y)
+    {
+        var position = new CheckerBoardPosition(x, y);
+        Assert.Equal(y, position.Y);
+    }
+    [Theory]
+    [InlineData("A1")]
+    [InlineData("C6")]
+    [InlineData("B8")]
+    public void Parse_ValidPosition_ReturnCorrectPosition(string post)
+    {
+        var position = CheckerBoardPosition.Parse(post, null);
+        Assert.Equal(post, position.ToString());
+    }
+    [Theory]
+    [InlineData("A9")]
+    [InlineData("Z6")]
+    [InlineData("b8")]
+    [InlineData("29")]
+    [InlineData("GG")]
+    public void Parse_NotValidPosition_ReturnFormatException(string post)
+    {
+        Assert.Throws<FormatException>(() => CheckerBoardPosition.Parse(post, null));
+    }
+
 }
